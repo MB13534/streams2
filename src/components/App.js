45204fda956220c688f6f244e6fcad9529acafdb
Michael <michael.barry.email@gmail.com>
2020-11-17 09:09:04 -0700

    5 routes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,22 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
-const PageOne = () => {
-  return (
-    <div>
-      This is Page One <Link to="/pagetwo">Link to Page One</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      This is Page Two <Link to="/">Link to Page One</Link>
-    </div>
-  );
-};
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit" component={StreamEdit} />
+          <Route path="/streams/delete" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
         </>
       </BrowserRouter>
     </div>
