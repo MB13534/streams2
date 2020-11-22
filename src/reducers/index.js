1c74595bc7a9caf10e87142c09955fad03c91fbd
Michael <michael.barry.email@gmail.com>
2020-11-21 21:30:25 -0700

    ready to start API

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,11 @@
 import { combineReducers } from "redux";
+//reducer is a premade reducer from redux-form, change the name to formReducer
+import { reducer as formReducer } from "redux-form";
+
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
+  //the redux-form reducer must be called form
+  form: formReducer,
 });
