a9ded66bd7a4294ffd887ec12293f8fb76086608
Michael <michael.barry.email@gmail.com>
2020-11-27 12:51:22 -0700

    streams reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
   auth: authReducer,
+  streams: streamReducer,
   //the redux-form reducer must be called form
   form: formReducer,
 });
