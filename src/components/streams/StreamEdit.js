b5dc528d8de2823569e2d7f17fd1d6387039c44a
Michael <michael.barry.email@gmail.com>
2020-12-04 11:09:05 -0700

    ready to add the forms to edit streams

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { fetchStream } from "../../actions";
+
 //the history object is passed by props by the router component
-const StreamEdit = (props) => {
-  return (
-    <div>
-      {props.stream.title}
-      {props.stream.description}
-    </div>
-  );
-};
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {this.props.stream.title}
+          {this.props.stream.description}
+        </div>
+      );
+    }
+  }
+}
 
 //own props is a second variable in mapStateToProps, it has the props passed to the component(the history)
 const mapStateToProps = (state, ownProps) => {
@@ -18,4 +29,4 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
